27b3808b6cbee66c781bd4ceab3113066441a40a
muxingci <3580593887@qq.com>
2025-04-27 21:38:06 +0800

    fix

--- train.py
+++ train.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2025/4/27 09:20:57
-# @Author  : 墨烟行(GitHub UserName: CloudSwordSage)
-# @File    : train.py
-# @Desc    :
-
 import os
 from ultralytics import YOLO
 import multiprocessing
+import torch
+
+torch.cuda.empty_cache()
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 import torch
 torch.cuda.empty_cache()
@@ -19,7 +17,26 @@ if __name__ == '__main__':
 
     model = YOLO(model_path)
 
-    model.train(data='./datasets/data.yaml', epochs=2)
+    # model.train(data='./datasets/data.yaml', epochs=2)
+    model.train(
+        data='./datasets/data.yaml',
+        epochs=400,
+        flipud=0.5,
+        fliplr=0.5,
+        mosaic=1.0,
+        degrees=0.0,
+        translate=0.1,
+        scale=0.5,
+        shear=0.0,
+        perspective=0.0,
+        mixup=0.0,
+        lr0=0.01,
+        lrf=0.01,
+        momentum=0.937,
+        weight_decay=0.0005
+    )
+
+    torch.cuda.empty_cache()
 
     torch.cuda.empty_cache()
 
